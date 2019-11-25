e96ff100ca71119074a28da58a7ae6d64a6955bc
Michael Winkworth <mwinkwo@btinternet.com>
2019-11-25 13:56:23 +0000

    Add comments

--- mapper.py
+++ mapper.py
@@ -4,21 +4,35 @@
 import sys
 from datetime import datetime
 
-# Either UK000056225 (Oxford) or UK000003377 (Waddington)
+# The ID of the weather station / location to gather values for.
+# Can either be UK000056225 (Oxford) or UK000003377 (Waddington).
 station_id_to_capture = 'UK000056225'
 
+# For each line in the input CSV file (passed into standard input)
 for line in sys.stdin:
+    # Remove trailing whitespace and split by commas into individual fields
     line = line.strip()
     fields = line.split(',')
+    # Give names to the important fields so that they are more human readable
     station_id = fields[0]
     measure = fields[2]
     quality = fields[5]
 
+    # We only want to send this line to the reducer if:
+    #   The location is the same as the one above
+    #   The measurement is either a TMAX measurement or TMIN measurement
+    #   The measurement hasn't failed any quality checks (i.e. the quality flag is blank)
     if station_id == station_id_to_capture and (measure == 'TMAX' or measure == 'TMIN') and quality == '':
+        # Get the date of the measurement. The date is parsed and then converted into a proleptic Gregorian ordinal
+        # (https://docs.python.org/2/library/datetime.html#datetime.date.toordinal), effectively an integer representing the date.
+        # This is done because it is easier to work with than date strings.
         date_string = fields[1]
         date = datetime.strptime(date_string, '%Y%m%d')
         date_ordinal = date.toordinal()
+        # Get the value of the measurement, which in this case will be the temperature, and convert it to an integer.
+        # Note that at this point the temperatures are still whole numbers in tenths of degrees.
         temp_string = fields[3]
         temp = int(fields[3])
 
+        # Output a key value pair of the date and the temperature to the reducer, separated by a tab character.
         print('%d\t%d' % (date_ordinal, temp))
--- reducer.py
+++ reducer.py
@@ -1,36 +1,54 @@
 #!/usr/bin/env python
 """reducer.py"""
 
+# Required for Python 3 style division
 from __future__ import division
 import sys
 
+# Set the current date to an initial value. This initial value is the proleptic Gregorian ordinal
+# (https://docs.python.org/2/library/datetime.html#datetime.date.toordinal) of the date 2018-1-1.
 current_date = 736695
 current_temp1 = None
 current_temp2 = None
 
+# For each line given by the mapper (passed into standard input)
 for line in sys.stdin:
+    # Remove trailing whitespace and split by the tab character to get the date temperature key-value pair, making sure to convert them to integers
     line = line.strip()
     date_string, temp_string = line.split('\t', 1)
     date = int(date_string)
     temp = int(temp_string)
 
+    # While the current date is not equal to the date from the key-value pair
+    # i.e. the date from the key-value pair has moved onto a new date from the previous key-value pair
     while current_date != date:
+        # If either of the current temperatures are not set, then output the default value (-99)
+        # i.e. a date does not have two temperature readings for TMIN and TMAX
         if not (current_temp1 and current_temp2):
             print('-99')
+        # Clear out the current temperatures and move onto the next day in the date
         current_temp1 = None
         current_temp2 = None
         current_date += 1
 
+    # If there is already a current temperature, i.e. this is the second temperature of a TMIN and TMAX pair
     if current_temp1:
+        # If there are already two temperatures, raise an error as this should not happen
         if current_temp2:
             raise AssertionError('There are more than two temperatures for the following date: %s' % (current_date))
 
+        # Set the temperature of the key-value pair as the second temperature
         current_temp2 = temp
+        # Calculate the temperature difference by taking the absolute difference between the two temperatures and dividing them by 10
+        # Division by 10 happens to convert from tenths of degrees into whole degrees (making the value now a float)
         # Note: this method assumes that TMAX will always be greater than or equal to TMIN
         current_temp_diff = abs(current_temp1 - current_temp2) / 10
+        # Output the temperature difference formatted to 1 d.p.
         print('%.1f' % (current_temp_diff))
+    # Otherwise, just set the temperature of the key-value pair as the first temperature
     else:
         current_temp1 = temp
 
+# If there is another default value (-99) that needs to be output after the loop, it is output here
 if not (current_temp1 and current_temp2):
     print('-99')
